47549e99e894dab4925a97a35309496c656eb160
Simon Puschmann <git.absolute576@passinbox.com>
2026-09-13 13:18:31 +0200

    add ft_isalpha, ft_isdigit, ft_isalnum, ft_isascii and ft_isprint

---
+++ it_is_what_it_is.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   it_is_what_it_is.c                                :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: spuschma <spuschma@student.42vienna.com>  #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/09/13 12:44:33 by spuschma         #+#    #+#              */
+/*   Updated: 2026/09/13 12:52:38 by spuschma        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalpha(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
+
+int	ft_isascii(int c)
+{
+	return (c >= 0 && c <= 127);
+}
+
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
