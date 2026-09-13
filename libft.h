79d88f870d037c7a5b6c3f7adf882e78f59524fc
Simon Puschmann <git.absolute576@passinbox.com>
2026-09-13 15:29:14 +0200

    add header file

---
+++ libft.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   libft.h                                           :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: spuschma <spuschma@student.42vienna.com>  #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/09/13 15:37:09 by spuschma         #+#    #+#              */
+/*   Updated: 2026/09/13 16:24:14 by spuschma        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_isdigit(int c);
+
+int		ft_isalnum(int c);
+
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+int		ft_isalpha(int c);
+
+#endif
