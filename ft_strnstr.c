b8f2778888a3d02f8137983def299bf8cd3b4015
Simon Puschmann <git.absolute576@passinbox.com>
2026-09-16 21:23:39 +0200

    fix ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: spuschma <spuschma@student.42vienna.com>  #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/09/13 21:11:38 by spuschma         #+#    #+#              */
-/*   Updated: 2026/09/16 21:07:05 by spuschma        ###   ########.fr        */
+/*   Updated: 2026/09/16 21:36:31 by spuschma        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	l_size;
 	size_t	j;
 
-	if (len == 0)
-		return ((char *)big);
-	i = 0;
 	l_size = ft_strlen(little);
-	while (i + l_size < len)
+	if (l_size == 0)
+		return ((char *)big);
+	if (len < 1)
+		return (NULL);
+	i = 0;
+	while (big[i] && i + l_size <= len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] && j < l_size - 1)
+		while (big[i] && big[i + j] == little[j] && j < l_size)
 			j++;
-		if (j == l_size - 1)
+		if (j == l_size)
 			return ((char *)big + i);
+		i++;
 	}
 	return (NULL);
 }
