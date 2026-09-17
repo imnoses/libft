550765dca4762d3beef50d56380ff9657f0673a4
Simon Puschmann <git.absolute576@passinbox.com>
2026-09-17 13:06:49 +0200

    add ft_lstlast

--- Makefile
+++ Makefile
@@ -37,6 +37,7 @@ SRCS		= $(SRC_DIR)/ft_atoi.c \
 			  $(SRC_DIR)/ft_isprint.c \
 			  $(SRC_DIR)/ft_itoa.c \
 			  $(SRC_DIR)/ft_lstadd_front.c \
+			  $(SRC_DIR)/ft_lstlast.c \
 			  $(SRC_DIR)/ft_lstnew.c \
 			  $(SRC_DIR)/ft_lstsize.c \
 			  $(SRC_DIR)/ft_memchr.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: spuschma <spuschma@student.42vienna.com>  #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/09/13 15:37:09 by spuschma         #+#    #+#              */
-/*   Updated: 2026/09/17 13:03:55 by spuschma        ###   ########.fr        */
+/*   Updated: 2026/09/17 13:08:57 by spuschma        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,8 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 
 unsigned int		ft_lstsize(t_list *lst);
+
+t_list				*ft_lstlast(t_list *lst);
 
 int					ft_isalpha(int c);
 
