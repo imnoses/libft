0ffd7c572f156a76d274509bada4b2ffef8753b0
Simon Puschmann <git.absolute576@passinbox.com>
2026-09-18 10:30:47 +0200

    copy glibc doc comments for the functions where it applies

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: spuschma <spuschma@student.42vienna.com>  #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/09/13 15:37:09 by spuschma         #+#    #+#              */
-/*   Updated: 2026/09/17 13:56:05 by spuschma        ###   ########.fr        */
+/*   Updated: 2026/09/18 10:43:46 by spuschma        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,39 +29,57 @@ int					ft_isascii(int c);
 
 int					ft_isprint(int c);
 
+/** Return the length of S.  */
 size_t				ft_strlen(const char *s);
 
+/** Set N bytes of S to 0.  */
 void				ft_bzero(void *s, size_t n);
 
+/** Copy N bytes of SRC to DEST.  */
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 
+/** Copy N bytes of SRC to DEST, guaranteeing
+   correct behavior for overlapping strings.  */
 void				*ft_memmove(void *dest, const void *src, size_t n);
 
+/** Copy at most SIZE - 1 characters from SRC to DEST.  */
 size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 
+/** Append SRC to DEST, possibly with truncation to keep the total size
+   below SIZE.  */
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
 
+/** Return the lowercase version of C.  */
 int					ft_tolower(int c);
 
+/** Return the uppercase version of C.  */
 int					ft_toupper(int c);
 
+/** Find the first occurrence of C in S.  */
 char				*ft_strchr(const char *s, int c);
 
+/** Find the last occurrence of C in S.  */
 char				*ft_strrchr(const char *s, int c);
 
+/** Compare N characters of S1 and S2.  */
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 
+/** Search N bytes of S for C.  */
 void				*ft_memchr(const void *s, int c, size_t n);
 
+/** Compare N bytes of S1 and S2.  */
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char				*ft_strnstr(const char *big, const char *little,
 						size_t len);
 
+/** Convert a string to an integer.  */
 int					ft_atoi(const char *nptr);
 
+/** Allocate N elements of SIZE bytes each, all initialized to 0.  */
 void				*ft_calloc(size_t n, size_t size);
 
+/** Duplicate S, returning an identical malloc'd string.  */
 char				*ft_strdup(const char *s);
 
 char				*ft_substr(char const *s, unsigned int start, size_t len);
@@ -86,6 +104,7 @@ void				ft_putendl_fd(char *s, int fd);
 
 void				ft_putnbr_fd(int n, int fd);
 
+/** Set N bytes of S to C.  */
 void				*ft_memset(void *s, int c, size_t n);
 
 t_list				*ft_lstnew(void *content);
